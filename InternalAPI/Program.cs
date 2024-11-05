3ed9a26c67c7c5cb80dfe59c26b8454b421b6bd1
Kiran B Chitari <kiran.chitari@flexoffers.com>
2024-11-05 09:11:08 +0530

    Added docker file

--- InternalAPI/Program.cs
+++ InternalAPI/Program.cs
@@ -1,7 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -17,9 +16,13 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
+// Map the controllers
 app.MapControllers();
+
+// Set the application to listen on the port defined by Vercel
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080"; // Default to 8080 if PORT is not set
+app.Urls.Add($"http://*:{port}"); // Listen on the specified port
 
 app.Run();
